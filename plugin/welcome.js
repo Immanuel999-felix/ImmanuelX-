d729953c89d79d1357822cc6500351a37a6ccbeb
Immanuel999-felix <emmanuelfelix829@gmail.com>
2024-12-24 08:59:23 +0100

    Delete plugin directory

--- plugin/welcome.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (conn, mek) => {
-    const { from, groupName, isGroup } = mek;
-    if (!isGroup) return;
-
-    conn.sendMessage(from, { text: `Welcome to ${groupName}!` });
-};
