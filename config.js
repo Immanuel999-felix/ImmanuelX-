fce6a0a7147e35fefa71fbbee286c8446c0b73d4
Immanuel999-felix <emmanuelfelix829@gmail.com>
2024-12-24 08:25:23 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    SESSION_ID: process.env.SESSION_ID,
-    ownerNumber: [process.env.OWNER_NUMBER],
-    botName: process.env.BOT_NAME || 'Immanuel999-bot-MD',
-    prefix: process.env.PREFIX || '.'
-};
-
